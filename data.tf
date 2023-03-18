26729632c930cf68c47a21b633f7946540012697
EreminAnton <mobessona2@gmail.com>
2023-03-18 17:54:48 +0500

    rm ssm parameters

--- data.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ssm_parameter" "slack_bot_token" {
-  name            = "/${local.name}/slack_bot_token"
-  with_decryption = true
-}
-
-data "aws_ssm_parameter" "slack_signing_secret" {
-  name            = "/${local.name}/slack_signing_secret"
-  with_decryption = true
-}
