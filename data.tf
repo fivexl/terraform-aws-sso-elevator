96374ed582abaabc61d5982332ed55ae853f8bfd
Vladimir Samoylov <51059484+cageyv@users.noreply.github.com>
2023-04-13 01:25:28 +0700

    ci: localstack with terraform (#7)
    * ci: try localstack with terraform
    
    * fix ci
    
    * Update base.yml
    
    health-retries
    
    * add temp s3 backend config
    
    * try to fix temp s3 backend
    
    * drop validation for sso
    
    * ci: skip creating s3 backend
    
    * ci: put back config command
    
    * ci: add install poetry
    
    * ci fix
    
    * ci: add python 3.9
    
    * ci: localstack try with docker.sock volume

--- data.tf
+++ data.tf
@@ -1,3 +1,7 @@
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
+
+data "aws_ssoadmin_instances" "all" {
+  count = var.sso_instance_arn == "" ? 1 : 0
+}
---
+++ tests/localstack-backend.tf
@@ -0,0 +1,4 @@
+bucket         = "tf-state"
+key            = "terraform.tfstate"
+region         = "eu-central-1"
+dynamodb_table = "tf-state"
