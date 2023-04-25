e9079d65bf11e55e2002477c80c6f93959207e7d
Vladimir Samoylov <pingme@cageyv.dev>
2023-04-25 16:16:23 +0700

    add complete example

---
+++ examples/complete/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.54"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+  }
+}
