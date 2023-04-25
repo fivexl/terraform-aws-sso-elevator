ebdba102965e4699ea54d5eefc8dd02d30102974
EreminAnton <mobessona2@gmail.com>
2023-04-25 11:43:07 +0500

    upd python & lambda module version

--- layers.tf
+++ layers.tf
@@ -1,16 +1,16 @@
 module "powertools_pydantic" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "4.10.1"
+  version = "4.16.0"
 
   create_layer    = true
   create_function = false
   layer_name      = "powertools-pydantic"
   description     = "AWS Lambda Powertools with Pydantic"
 
-  compatible_runtimes = ["python3.9"]
+  compatible_runtimes = ["python3.10"]
   build_in_docker     = var.build_in_docker
-  runtime             = "python3.9"
-  docker_image        = "build-python3.9-poetry"
+  runtime             = "python3.10"
+  docker_image        = "build-python3.10-poetry"
   docker_file         = "${path.module}/src/docker/Dockerfile"
   source_path = [{
     poetry_install = true
@@ -22,17 +22,17 @@ module "powertools_pydantic" {
 
 module "slack_bolt" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "4.10.1"
+  version = "4.16.0"
 
   create_layer    = true
   create_function = false
   layer_name      = "python-slack-bolt"
   description     = "A framework that makes Slack app development fast and straight-forward."
 
-  compatible_runtimes = ["python3.9"]
+  compatible_runtimes = ["python3.10"]
   build_in_docker     = var.build_in_docker
-  runtime             = "python3.9"
-  docker_image        = "build-python3.9-poetry"
+  runtime             = "python3.10"
+  docker_image        = "build-python3.10-poetry"
   docker_file         = "${path.module}/src/docker/Dockerfile"
   source_path = [{
     poetry_install = true
@@ -44,17 +44,17 @@ module "slack_bolt" {
 
 module "python_boto3" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "4.10.1"
+  version = "4.16.0"
 
   create_layer    = true
   create_function = false
   layer_name      = "python-boto3"
   description     = "Python Boto3."
 
-  compatible_runtimes = ["python3.9"]
+  compatible_runtimes = ["python3.10"]
   build_in_docker     = var.build_in_docker
-  runtime             = "python3.9"
-  docker_image        = "build-python3.9-poetry"
+  runtime             = "python3.10"
+  docker_image        = "build-python3.10-poetry"
   docker_file         = "${path.module}/src/docker/Dockerfile"
   source_path = [{
     poetry_install = true
