3b701dadc474a3e05c7e143eeae5727148d8ec65
EreminAnton <mobessona2@gmail.com>
2023-04-27 13:09:04 +0500

    fix poetry deploy

--- layers.tf
+++ layers.tf
@@ -1,11 +1,11 @@
-module "powertools_pydantic" {
+module "sso_elevator_dependencies" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "4.16.0"
 
   create_layer    = true
   create_function = false
-  layer_name      = "powertools-pydantic"
-  description     = "AWS Lambda Powertools with Pydantic"
+  layer_name      = "sso_elevator_dependencies"
+  description     = "powertools-pydantic/boto3/slack_bolt"
 
   compatible_runtimes = ["python3.10"]
   build_in_docker     = var.build_in_docker
@@ -14,51 +14,7 @@ module "powertools_pydantic" {
   docker_file         = "${path.module}/src/docker/Dockerfile"
   source_path = [{
     poetry_install = true
-    path           = "${path.module}/layers/powertools-pydantic"
-    patterns       = ["!python/.venv/.*"]
-    prefix_in_zip  = "python"
-  }]
-}
-
-module "slack_bolt" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "4.16.0"
-
-  create_layer    = true
-  create_function = false
-  layer_name      = "python-slack-bolt"
-  description     = "A framework that makes Slack app development fast and straight-forward."
-
-  compatible_runtimes = ["python3.10"]
-  build_in_docker     = var.build_in_docker
-  runtime             = "python3.10"
-  docker_image        = "lambda/python:3.10"
-  docker_file         = "${path.module}/src/docker/Dockerfile"
-  source_path = [{
-    poetry_install = true
-    path           = "${path.module}/layers/python-slack-bolt"
-    patterns       = ["!python/.venv/.*"]
-    prefix_in_zip  = "python"
-  }]
-}
-
-module "python_boto3" {
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "4.16.0"
-
-  create_layer    = true
-  create_function = false
-  layer_name      = "python-boto3"
-  description     = "Python Boto3."
-
-  compatible_runtimes = ["python3.10"]
-  build_in_docker     = var.build_in_docker
-  runtime             = "python3.10"
-  docker_image        = "lambda/python:3.10"
-  docker_file         = "${path.module}/src/docker/Dockerfile"
-  source_path = [{
-    poetry_install = true
-    path           = "${path.module}/layers/python-boto3"
+    path           = "${path.module}/layer"
     patterns       = ["!python/.venv/.*"]
     prefix_in_zip  = "python"
   }]
