8769e9bb62a598daf80fc9dc47bb936e28abd003
Eremin <haru.eaa@gmail.com>
2023-05-01 14:26:06 +0500

    test access control

---
+++ src/tests/test_access_control.py
@@ -0,0 +1,331 @@
+import pytest
+
+from access_control import AccessRequestDecision, ApproveRequestDecision, DecisionReason, make_decision_on_approve_request, make_decision_on_access_request
+from statement import Statement
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        {  # approval is not required
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["*"],
+                                "permission_set": ["*"],
+                                "approval_is_not_required": True,
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "anybody@example.com",
+            },
+            "out": AccessRequestDecision(
+                grant=True,
+                reason=DecisionReason.ApprovalNotRequired,
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["*"],
+                                "permission_set": ["*"],
+                                "approval_is_not_required": True,
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+        {  # requester is not an approver
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["one@example.com"],
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "second@example.com",
+            },
+            "out": AccessRequestDecision(
+                grant=False,
+                reason=DecisionReason.RequiresApproval,
+                approvers=frozenset(["one@example.com"]),
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["one@example.com"],
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+        {  # self approval is allowed and requester is approver
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["*"],
+                                "permission_set": ["*"],
+                                "approvers": ["approver@example.com"],
+                                "allow_self_approval": True,
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "approver@example.com",
+            },
+            "out": AccessRequestDecision(
+                grant=True,
+                reason=DecisionReason.SelfApproval,
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["*"],
+                                "permission_set": ["*"],
+                                "approvers": ["approver@example.com"],
+                                "allow_self_approval": True,
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+        {  # no approvers
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["*"],
+                                "permission_set": ["*"],
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "example@example.com",
+            },
+            "out": AccessRequestDecision(
+                grant=False,
+                reason=DecisionReason.NoApprovers,
+                based_on_statements=frozenset(
+                    frozenset(
+                        [
+                            Statement.parse_obj(
+                                {
+                                    "resource_type": "Account",
+                                    "resource": ["*"],
+                                    "permission_set": ["*"],
+                                }
+                            )
+                        ]
+                    )
+                ),
+            ),
+        },
+        {  #  requester is an approver, but self approval is not allowed
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "approver@example.com",
+            },
+            "out": AccessRequestDecision(
+                grant=False,
+                reason=DecisionReason.NoApprovers,
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+        {  # requester is an approver, self approval is not allowed, but there are other approvers
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["requester@example.com", "approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "requester@example.com",
+            },
+            "out": AccessRequestDecision(
+                grant=False,
+                reason=DecisionReason.RequiresApproval,
+                approvers=frozenset(["approver@example.com"]),
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["requester@example.com", "approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+    ],
+)
+def test_make_access_decision(test_case):
+    assert make_decision_on_access_request(**test_case["in"]) == test_case["out"]
+
+
+@pytest.mark.parametrize(
+    "test_case",
+    [
+        {  # approver is approver
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "requester@example.com",
+                "approver_email": "approver@example.com",
+            },
+            "out": ApproveRequestDecision(
+                permit=True,
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+        {  # approver is approver but self approval is not allowed
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                                "allow_self_approval": False,
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "111111111111",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "approver@example.com",
+                "approver_email": "approver@example.com",
+            },
+            "out": ApproveRequestDecision(
+                permit=False,
+                based_on_statements=frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+            ),
+        },
+        {  # approver is not an approver
+            "in": {
+                "statements": frozenset(
+                    [
+                        Statement.parse_obj(
+                            {
+                                "resource_type": "Account",
+                                "resource": ["111111111111"],
+                                "permission_set": ["AdministratorAccess"],
+                                "approvers": ["approver@example.com"],
+                            }
+                        )
+                    ]
+                ),
+                "account_id": "222222222222",
+                "permission_set_name": "AdministratorAccess",
+                "requester_email": "requester@example.com",
+                "approver_email": "notapprover@example.com",
+            },
+            "out": ApproveRequestDecision(
+                permit=False,
+                based_on_statements=frozenset(),
+            ),
+        },
+    ],
+)
+def test_can_approve_request(test_case):
+    assert make_decision_on_approve_request(**test_case["in"]) == test_case["out"]
