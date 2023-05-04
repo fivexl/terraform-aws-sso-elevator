46394a4405015a790a89622adafa18884486c960
EreminAnton <mobessona2@gmail.com>
2023-05-04 19:50:31 +0500

    log permission duration to dynamodb

--- src/dynamodb.py
+++ src/dynamodb.py
@@ -1,11 +1,14 @@
 from dataclasses import asdict, dataclass
 from datetime import datetime
+from typing import Literal
 
 import boto3
 
 import config
 
 logger = config.get_logger(service="dynamodb")
+
+from datetime import timedelta
 
 
 @dataclass
@@ -19,12 +22,23 @@ class AuditEntry:
     approver_slack_id: str
     approver_email: str
     operation_type: str
+    permission_duration: timedelta | Literal["NA"]
 
 
 def log_operation(table_name: str, audit_entry: AuditEntry):
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(table_name)
     now = datetime.now()
-    audit_entry_with_time = asdict(audit_entry) | {"time": str(now), "timestamp": int(now.timestamp() * 1000)}
-    result = table.put_item(Item=audit_entry_with_time)
+    if isinstance(audit_entry.permission_duration, timedelta):
+        permission_duration = int(audit_entry.permission_duration.total_seconds())
+    else:
+        permission_duration = "NA"
+
+    audit_entry_dict = asdict(audit_entry) | {
+        "permission_duration": permission_duration,
+        "time": str(now),
+        "timestamp": int(now.timestamp() * 1000),
+    }
+
+    result = table.put_item(Item=audit_entry_dict)
     logger.debug("Audit entry posted to dynamodb", extra={"result": result, "table": table_name})
