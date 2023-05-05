ed1f713fcc03047e3692886ed014f09ffae3c86c
EreminAnton <mobessona2@gmail.com>
2023-05-05 12:55:51 +0500

    fmt examples/main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -29,13 +29,13 @@ module "aws_sso_elevator" {
   source                           = "../.."
   aws_sns_topic_subscription_email = "email@gmail.com"
 
-  slack_signing_secret         = data.aws_ssm_parameter.sso_elevator_slack_signing_secret.value
-  slack_bot_token              = data.aws_ssm_parameter.sso_elevator_slack_bot_token.value
-  slack_channel_id             = "***********"
-  schedule_expression          = "cron(0 23 * * ? *)" # revoke access schedule expression
-  schedule_expression_for_check_on_inconsistency = "rate(1 hour)" 
-  build_in_docker              = true
-  revoker_post_update_to_slack = true
+  slack_signing_secret                           = data.aws_ssm_parameter.sso_elevator_slack_signing_secret.value
+  slack_bot_token                                = data.aws_ssm_parameter.sso_elevator_slack_bot_token.value
+  slack_channel_id                               = "***********"
+  schedule_expression                            = "cron(0 23 * * ? *)" # revoke access schedule expression
+  schedule_expression_for_check_on_inconsistency = "rate(1 hour)"
+  build_in_docker                                = true
+  revoker_post_update_to_slack                   = true
 
   sso_instance_arn = one(data.aws_ssoadmin_instances.this.arns)
 
