b4cbc48b759f0ae9a738f99af258478963a0f401
Anton Eremin <mobessona2@gmail.com>
2023-05-11 11:41:27 +0500

    upd tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -12,10 +12,11 @@ def pytest_sessionstart(session):
         "post_update_to_slack": "true",
         "slack_channel_id": "x",
         "slack_bot_token": "x",
-        "dynamodb_table_name": "x",
         "sso_instance_arn": "x",
         "log_level": "DEBUG",
         "slack_app_log_level": "INFO",
+        "s3_bucket_for_audit_entry_name": "x",
+        "s3_bucket_prefix_for_partitions": "x",
     }
     os.environ |= mock_env
 
