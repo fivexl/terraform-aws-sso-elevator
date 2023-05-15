5e743e7dbd13efb0b2d7c24f9f8b9c6ba0b99e04
Anton Eremin <mobessona2@gmail.com>
2023-05-15 22:58:34 +0500

    optional python_version_vheck for build_in_docker

--- check_python_version.tf
+++ check_python_version.tf
@@ -1,6 +1,7 @@
 # When the local Python version is not 3.10.10, pip will not install some packages.
 # Therefore, we need to check the Python version before deployment.
 resource "null_resource" "version_check" {
+  count = var.build_in_docker == true ? 0 : 1
   provisioner "local-exec" {
     command = <<-EOF
       version=$(python --version 2>&1 | awk '{print $2}')
