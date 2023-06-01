d03b3194a61f01bf19e5b17e181844e65b2164a3
Anton Eremin <mobessona2@gmail.com>
2023-06-01 17:58:01 +0500

    fmt & docs

--- src/check_python_version.py
+++ src/check_python_version.py
@@ -11,6 +11,8 @@ current_version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.versi
 
 if current_version != required_version:
     # Write the error message to stderr
-    sys.stderr.write(f"Local python version is incorrect: {current_version}. Required version is {required_version}. Please clean 'builds', and then use docker for deployment, or destroy and re-create sso_elevator with the correct python version.")
+    sys.stderr.write(
+        f"Local python version is incorrect: {current_version}. Required version is {required_version}. Please clean 'builds', and then use docker for deployment, or destroy and re-create sso_elevator with the correct python version."  # noqa: E501
+    )
     # Exit with a status code of 1, indicating failure
     sys.exit(1)
