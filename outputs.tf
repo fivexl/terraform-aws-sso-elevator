ac314f010f0111d250a2e9385c7cdfc5c66681bb
zhelezovartem <zhelezovartem@gmail.com>
2024-02-07 20:55:07 +0600

    add sso_elevator_bucket_id output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "lambda_function_url" {
   description = "value for the access_requester lambda function URL"
   value       = module.access_requester_slack_handler.lambda_function_url
 }
+
+output "sso_elevator_bucket_id" {
+  description = "The name of the SSO elevator bucket."
+  value       = var.s3_name_of_the_existing_bucket == "" ? module.sso_elevator_bucket[0].s3_bucket_id : null
+}
