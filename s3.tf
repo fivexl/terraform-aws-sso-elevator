8a07d70115353353051f8bb6a6b61c15df59a7e2
Anton Eremin <mobessona2@gmail.com>
2024-06-10 11:49:12 +0500

    feat: use baseline for Audit bucket creation

--- s3.tf
+++ s3.tf
@@ -1,17 +1,9 @@
-module "sso_elevator_bucket" {
+module "audit_bucket" {
   count   = var.s3_name_of_the_existing_bucket == "" ? 1 : 0
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.10.1"
-
-  bucket = local.s3_bucket_name
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  source  = "fivexl/account-baseline/aws//modules/s3_baseline"
+  version = "1.3.2"
+  
+  bucket_name = local.s3_bucket_name
 
   versioning = {
     enabled    = true
@@ -21,12 +13,5 @@ module "sso_elevator_bucket" {
   object_lock_enabled = var.s3_object_lock
 
   object_lock_configuration = var.s3_object_lock ? var.s3_object_lock_configuration : null
-
   logging = var.s3_logging
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
-
