5fa7192c9d2d590c3985e47ca3a1176d5e1777ec
Anton Eremin <mobessona2@gmail.com>
2024-06-11 11:45:51 +0500

    fix: fmt

--- moved.tf
+++ moved.tf
@@ -1,5 +1,5 @@
 # Start using s3 baseline for audit bucket
 moved {
   from = module.sso_elevator_bucket[0]
-  to = module.audit_bucket[0].module.bucket_baseline[0]
+  to   = module.audit_bucket[0].module.bucket_baseline[0]
 }
