b4be2cff80283d98c70534671db31a8458acf8ce
Anton Eremin <mobessona2@gmail.com>
2024-06-11 11:45:51 +0500

    fix: fmt

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ module "audit_bucket" {
   count   = var.s3_name_of_the_existing_bucket == "" ? 1 : 0
   source  = "fivexl/account-baseline/aws//modules/s3_baseline"
   version = "1.3.2"
-  
+
   bucket_name = local.s3_bucket_name
 
   versioning = {
@@ -13,5 +13,5 @@ module "audit_bucket" {
   object_lock_enabled = var.s3_object_lock
 
   object_lock_configuration = var.s3_object_lock ? var.s3_object_lock_configuration : null
-  logging = var.s3_logging
+  logging                   = var.s3_logging
 }
