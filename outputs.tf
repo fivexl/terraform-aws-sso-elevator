d74b02e91dcb6939d5ca53101f612f14e11a0932
Anton Eremin <mobessona2@gmail.com>
2024-06-11 09:25:29 +0500

    fix: rename module in output

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "lambda_function_url" {
 
 output "sso_elevator_bucket_id" {
   description = "The name of the SSO elevator bucket."
-  value       = var.s3_name_of_the_existing_bucket == "" ? module.sso_elevator_bucket[0].s3_bucket_id : null
+  value       = var.s3_name_of_the_existing_bucket == "" ? module.audit_bucket[0].s3_bucket_id : null
 }
