e7a8251d583d2721d9feb013ef733df503176a0c
Anton Eremin <mobessona2@gmail.com>
2024-07-09 11:44:15 +0500

    docs: fix description

--- vars.tf
+++ vars.tf
@@ -4,7 +4,7 @@ variable "use_deprecated_lambda_url" {
   If false, the Lambda function will switch to using API Gateway for triggering.
 
   Although the Lambda URL will remain as the default for backward compatibility, it is recommended to switch to using API Gateway.
-  In future versions, this variable will default to false, and the ability to use the Lambda URL will eventually be removed.
+  In future versions ability to use the Lambda URL will eventually be removed.
   EOT
   type        = bool
   default     = false
