f981ee8e88352e2800103d2e3c6dfa0080d71190
Anton Eremin <mobessona2@gmail.com>
2025-02-21 17:14:19 +0500

    fix: get_max_duration_block limit to 99 so slack will not fail

--- src/slack_helpers.py
+++ src/slack_helpers.py
@@ -450,11 +450,16 @@ def get_message_from_timestamp(channel_id: str, message_ts: str, slack_client: s
 
     return None
 
-
+# Plain text object supports only 99 options
+# https://github.com/fivexl/terraform-aws-sso-elevator/issues/110
 def get_max_duration_block(cfg: config.Config) -> list[Option]:
+    max_increments = min(cfg.max_permissions_duration_time * 2, 99)
     return [
-        Option(text=PlainTextObject(text=f"{i // 2:02d}:{(i % 2) * 30:02d}"), value=f"{i // 2:02d}:{(i % 2) * 30:02d}")
-        for i in range(1, cfg.max_permissions_duration_time * 2 + 1)
+        Option(
+            text=PlainTextObject(text=f"{i // 2:02d}:{(i % 2) * 30:02d}"),
+            value=f"{i // 2:02d}:{(i % 2) * 30:02d}"
+        )
+        for i in range(1, max_increments + 1)
     ]
 
 
